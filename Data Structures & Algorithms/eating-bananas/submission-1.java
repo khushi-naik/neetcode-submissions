bba5cf3e38cd6cd709e9466ee95723df626879a0
khushi-naik <54225015+khushi-naik@users.noreply.github.com>
2026-07-29 18:01:02 -0400

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        //Arrays.sort(piles);
+        int low=1;
+        int high=0;
+        for(int i=0;i<piles.length;i++){
+            if(piles[i]>high){
+                high=piles[i];
+            }
+        }
+
+        int mid =0;
+        int res=high;
+        while(low<=high){
+            mid =(low+high)/2;
+            int currhours=0;
+            for(int i=0;i<piles.length;i++){
+                currhours+=Math.ceil((double)piles[i]/mid);
+            }
+            if(currhours>h){
+                low=mid+1;
+            }
+            else{
+                res = mid;
+                high=mid-1;
+            }
+            
+        }
+
+        return res;
+    }
+}
